d3e08955979286f4f4e09375b27ee64e792c8106
zhangwenwei <eastww@qq.com>
2023-09-18 02:08:17 +0800

    Enable two ways rs232 to communicate

--- src/CubeIDE/Core/Src/main.c
+++ src/CubeIDE/Core/Src/main.c
@@ -103,6 +103,7 @@ int main(void)
 
     /* USER CODE BEGIN 3 */
 	  bsp_test();
+	  HAL_UART_Transmit(&huart1, buffer, 10, 0xFFFF);
 	  HAL_UART_Transmit(&huart2, buffer, 10, 0xFFFF);
 	  HAL_Delay(1000);
   }
@@ -169,7 +170,7 @@ static void MX_USART1_UART_Init(void)
 
   /* USER CODE END USART1_Init 1 */
   huart1.Instance = USART1;
-  huart1.Init.BaudRate = 115200;
+  huart1.Init.BaudRate = 9600;
   huart1.Init.WordLength = UART_WORDLENGTH_8B;
   huart1.Init.StopBits = UART_STOPBITS_1;
   huart1.Init.Parity = UART_PARITY_NONE;
